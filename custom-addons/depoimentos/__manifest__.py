ca6c5c35b912bd9c97babf078e94ab27f31d29cc
Alisson Noli <despi.663@gmail.com>
2025-09-02 20:57:04 +0100

    Adiciona diretório custom-addons e módulo depoimentos

---
+++ custom-addons/depoimentos/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Módulo de Depoimentos",
+    "version": "17.0.1.0.0",
+    "category": "Custom",
+    "summary": "Módulo de Depoimentos.",
+    "description": "Módulo de Depoimentos para Odoo 17.",
+    "depends": ["base"],
+    "data": [],
+    "installable": True,
+    "application": False,
+}
